038bbf9c4c31a71fedcf5b12dc3bd14c3938867b
herzliyaaa <herzliabarangan@gmail.com>
2023-02-08 09:00:21 +0800

    fix: Fixed duplication of products

--- src/data-access/quotation/quotation.query.js
+++ src/data-access/quotation/quotation.query.js
@@ -49,14 +49,12 @@ const quotationData = ({ dbs }) => {
     const sql = `SELECT
     Q.quotation_id, S.service_id,  SK.sku_id,  C.customer_id, QL.qty, QL.amount, SK.unit, SK.cost,
     PP.printname, PP.barcode, PC.serial_number,
-    PC.brand_name, PC.model, PC.color, SI.service_name, SI.unit as service_unit, SI.cost as service_cost,
+    PC.brand_name, PC.model, PC.color, 
     C.name, C.contact_number, C.address, Q.date_transaction, Q.status
     FROM quotation Q
     INNER JOIN quotation_line QL ON QL.quotation_id = Q.quotation_id
     LEFT OUTER JOIN sku SK ON SK.sku_id = QL.sku_id
     LEFT OUTER JOIN service S ON S.service_id = Q.service_id
-    LEFT OUTER JOIN service_line SL ON SL.service_id =  S.service_id
-    LEFT OUTER JOIN service_item SI ON SI.service_item_id = SL.service_item_id
     LEFT OUTER JOIN product_parts PP ON SK.sku_id = PP.sku_id
     LEFT OUTER JOIN product_car PC ON PC.sku_id = SK.sku_id
     INNER JOIN customer C ON C.customer_id = Q.customer_id where Q.quotation_id = $1;`;
